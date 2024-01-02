dce168d7da0b3ccdd5da4fb35a4a3a9c33685843
svesTECHEVENT <144025248+svesTECHEVENT@users.noreply.github.com>
2024-01-01 21:42:32 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const io = socketIo(server,{
 });
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://travalapp:travalapp@cluster0.oz5xxmc.mongodb.net/');
+mongoose.connect('mongodb+srv://travalapp:travalapp@cluster0.oz5xxmc.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Use bodyParser middleware to parse JSON
 app.use(bodyParser.json());
